22456823705483ec529f74a9e45ca843b675bf25
Mayedl10 <danielmayer0708@gmail.com>
2024-10-16 18:10:24 +0200

    fixed issue #17

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,16 @@
 #include "fileIO.hpp"
 #include "lexer.hpp"
 
-#define pressEnterToContinue do { \
-        std::cout << "Press enter to close this window."; \
-        std::cin.get(); \
-        std::cout << std::endl; \
-    } while (0)
+void press_enter_to_continue() {
+    std::cout << "Press enter to continue";
+    std::cin.get();
+    std::cout << std::endl;
+}
 
 int main(int argc, char* argv[]) {
 
-    std::string filename = "file.bfil";
-    std::string oFilename = "out";
-    std::string oFileExt = "bf";
-    std::string filePath = "";
-    std::string oFilePath = "";
+    std::string inputFile = "";
+    std::string outputFile = "";
     bool keepWindowOpen = false;
 
     if (argc <= 1) {
@@ -30,58 +27,50 @@ int main(int argc, char* argv[]) {
 
     for (int i = 1; i < argc; i++) {
 
-        if (strcmp(argv[i], "-f") == 0) {
-            i++;
-            filename = argv[i];
+        if (strcmp(argv[i], "-i") == 0) {
+            inputFile = argv[++i];
         }
-        
+
         if (strcmp(argv[i], "-o") == 0) {
-
-            i++;
-            oFilename = argv[i];
-        }
-        
-        if (strcmp(argv[i], "-O") == 0) {
-
-            i++;
-            oFileExt = argv[i];
-        }
-        
-        if (strcmp(argv[i], "-p") == 0) {
-            i++;
-            filePath = argv[i];
-        }
-        
-        if (strcmp(argv[i], "-P") == 0) {
-
-            i++;
-            oFilePath = argv[i];
+            outputFile = argv[++i];
         }
 
         if (strcmp(argv[i], "-k") == 0) {
-
             keepWindowOpen = true;
-
         }
+
+        if (strcmp(argv[i], "-O") == 0) {
+            std::cout << "[Compiler flag -O is yet to be implemented. No optimisations will occur.]" << std::endl;
+        }
+
+        if (strcmp(argv[i], "-h") == 0) {
+            std::cout
+            << "-h help\n"
+            << "-i input file path (eg. \"/foo/bar/test.bfil\")\n"
+            << "-o output filename (eg. \"/foo/bar/test.bf\")\n"
+            << "-k keep the window open after compilation is finished\n"
+            << "-O optimise (not yet implemented, see issue #14)\n\n"
+            << "If you encounter any bugs or have feature suggestions, please visit https://github.com/Mayedl10/BFIL/"
+            << std::endl;
+        }
+
     }
 
 
-    if (oFilePath == "") {
-
-        oFilePath = filePath;
+    if (outputFile == "") {
+        std::cout << " [Please provide a file to write to]" << std::endl;
     }
 
-    std::string iFile = filePath + "\\" + filename;
-    std::string oFile = oFilePath + "\\" + oFilename + "." + oFileExt;
     std::string fileContent;
 
     try
     {
-        fileContent = getFileContentAsString(iFile);
+        fileContent = getFileContentAsString(inputFile);
     }
 
     catch(const std::exception& e)
     {
+        std::cout << "[Couldn't read input file]" << std::endl;
         return 1;
     }
     
@@ -89,21 +78,20 @@ int main(int argc, char* argv[]) {
     std::string compiledCode = compile(tokens);
 
     if (compiledCode != "do not output") {
-        writeToFile(oFile, "", compiledCode);
+        writeToFile(outputFile, "", compiledCode);
         
     } else {
         std::cout << "------------------------------------------------------------------\n|Code will not be saved to file due to errors during compilation.|\n------------------------------------------------------------------" << std::endl;
         
         if (keepWindowOpen) {
             
-            pressEnterToContinue;
+            press_enter_to_continue();
 
         }
         
         return 1;
     }
 
-    pressEnterToContinue;
 
     return 0;
 }
